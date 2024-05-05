3922e96da7ed904a814a4da4ebb2f31def05dc52
DancingSnow <1121149616@qq.com>
2024-05-05 19:31:54 +0800

    完善贴图配方

--- src/main/java/cn/dancingsnow/bigger_ae2/init/ModCreativeTab.java
+++ src/main/java/cn/dancingsnow/bigger_ae2/init/ModCreativeTab.java
@@ -13,9 +13,12 @@ public class ModCreativeTab {
         .defaultCreativeTab("bigger_ae2", builder -> builder
             .icon(ModItems.SINGULARITY_ITEM_CELL::asStack)
             .displayItems((parameters, output) -> {
+                output.accept(ModItems.ADVANCED_ITEM_CELL_HOUSING);
+                output.accept(ModItems.ADVANCED_FLUID_CELL_HOUSING);
                 output.accept(ModItems.QUANTUM_CELL_COMPONENT);
                 output.accept(ModItems.QUANTUM_ITEM_CELL);
                 output.accept(ModItems.QUANTUM_FLUID_CELL);
+                output.accept(ModItems.SINGULARITY_CELL_COMPONENT);
                 output.accept(ModItems.SINGULARITY_ITEM_CELL);
                 output.accept(ModItems.SINGULARITY_FLUID_CELL);
             })
--- src/main/java/cn/dancingsnow/bigger_ae2/item/cell/DigitalSingularityCellItem.java
+++ src/main/java/cn/dancingsnow/bigger_ae2/item/cell/DigitalSingularityCellItem.java
@@ -4,22 +4,31 @@ import appeng.api.config.FuzzyMode;
 import appeng.api.stacks.AEKey;
 import appeng.api.stacks.AEKeyType;
 import appeng.api.stacks.GenericStack;
+import appeng.api.storage.StorageCells;
 import appeng.api.storage.cells.ICellHandler;
 import appeng.api.storage.cells.ICellWorkbenchItem;
 import appeng.api.storage.cells.ISaveProvider;
 import appeng.api.upgrades.IUpgradeInventory;
 import appeng.api.upgrades.UpgradeInventories;
 import appeng.core.AEConfig;
+import appeng.core.localization.PlayerMessages;
 import appeng.core.localization.Tooltips;
 import appeng.items.AEBaseItem;
 import appeng.items.contents.CellConfig;
 import appeng.items.storage.StorageCellTooltipComponent;
 import appeng.util.ConfigInventory;
+import appeng.util.InteractionUtil;
 import lombok.Getter;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -34,9 +43,19 @@ public class DigitalSingularityCellItem extends AEBaseItem implements ICellWorkb
     public static final Handler HANDLER = new Handler();
     private final AEKeyType keyType;
 
-    public DigitalSingularityCellItem(Properties properties, AEKeyType keyType) {
+    private final ItemLike coreItem;
+    private final ItemLike housingItem;
+
+    public DigitalSingularityCellItem(
+        Properties properties,
+        AEKeyType keyType,
+        ItemLike coreItem,
+        ItemLike housingItem
+    ) {
         super(properties.stacksTo(1));
         this.keyType = keyType;
+        this.coreItem = coreItem;
+        this.housingItem = housingItem;
     }
 
 
@@ -103,6 +122,48 @@ public class DigitalSingularityCellItem extends AEBaseItem implements ICellWorkb
             }
         }
         return Optional.of(new StorageCellTooltipComponent(upgrades, content, false, true));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        this.disassembleDrive(player.getItemInHand(usedHand), level, player);
+        return new InteractionResultHolder<>(
+            InteractionResult.sidedSuccess(level.isClientSide()),
+            player.getItemInHand(usedHand)
+        );
+    }
+
+    private boolean disassembleDrive(ItemStack stack, Level level, Player player) {
+        if (InteractionUtil.isInAlternateUseMode(player)) {
+            if (level.isClientSide()) {
+                return false;
+            }
+
+            final Inventory playerInventory = player.getInventory();
+            var inv = StorageCells.getCellInventory(stack, null);
+            if (inv != null && playerInventory.getSelected() == stack) {
+                var list = inv.getAvailableStacks();
+                if (list.isEmpty()) {
+                    playerInventory.setItem(playerInventory.selected, ItemStack.EMPTY);
+
+                    // drop core
+                    playerInventory.placeItemBackInInventory(new ItemStack(coreItem));
+
+                    // drop upgrades
+                    for (var upgrade : this.getUpgrades(stack)) {
+                        playerInventory.placeItemBackInInventory(upgrade);
+                    }
+
+                    // drop empty storage cell case
+                    playerInventory.placeItemBackInInventory(new ItemStack(housingItem));
+
+                    return true;
+                } else {
+                    player.displayClientMessage(PlayerMessages.OnlyEmptyCellsCanBeDisassembled.text(), true);
+                }
+            }
+        }
+        return false;
     }
 
     public static class Handler implements ICellHandler {
